e1d8d26b84079fb2cdce0051d76d521bbbd7550b
Sam-xiexielaoshi <samagrasingh20032003@gmail.com>
2025-06-12 19:40:48 +0530

    err wrong choice

--- 2-express-tut/app.js
+++ 2-express-tut/app.js
@@ -3,14 +3,17 @@ const app = express();
 
 // req => middleware => res
 
-app.get('/', (req,res)=>{
-  res.send('Home Page')
-})
+app.get("/", (req, res) => {
+  const method = req.method;
+  const url = req.url;
+  const time = new Date().getFullYear();
+  console.log(method,url,time)
+  res.send("Home Page");
+});
 
-app.get('/about', (req,res)=>{
-  res.send('About Page')
-})
-
+app.get("/about", (req, res) => {
+  res.send("About Page");
+});
 
 app.listen(5000, () => {
   console.log("the server is running on port 5000");
